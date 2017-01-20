9f6b2a558cb1fa7fe11a4d9ea05e81c7afa04c93
Jelle van Dijk <vandijkjelle@gmail.com>
2017-01-20 23:42:16 +0100

    Middle island spawns waves

---
+++ GGJ2017/Assets/MiddleIslandBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleIslandBehaviour : MonoBehaviour {
+
+    public GameObject wave;
+    public float waveSpeed = 5;
+    public float timeLeft = 0;
+	// Use this for initialization
+	void Start () {     
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Wave();
+    }
+
+    void Wave()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            timeLeft = waveSpeed;
+            Instantiate(wave);
+        }
+    }
+}
---
+++ GGJ2017/Assets/WaveBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+    public float speed = 0.01f;
+    public float maxScale = 1f;
+    private float maxSize;
+
+	void Start () {
+        this.transform.localScale = Vector3.zero;
+        maxSize = GameObject.Find("PlayingField").GetComponent<SpriteRenderer>().bounds.size.x * maxScale;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Grow();
+	}
+
+    void Grow()
+    {
+        /*grows the wave*/
+        Vector3 scale = this.transform.localScale;
+        this.transform.localScale = new Vector3(scale.x + speed, scale.y + speed, scale.z + speed);
+
+        /* Destroys the wave if it reaches the max size */
+        if (this.GetComponent<SpriteRenderer>().bounds.size.x >= maxSize)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    
+}
