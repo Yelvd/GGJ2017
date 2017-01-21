660ac0649bf08adb5a64da8b83d44db1222a4018
Jelle van Dijk <vandijkjelle@gmail.com>
2017-01-21 02:58:41 +0100

    switching from islands works for 1 player need to add multy player support and switch needs to be togled

--- GGJ2017/Assets/Scripts/IslandBehavior.cs
+++ GGJ2017/Assets/Scripts/IslandBehavior.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IslandBehavior : MonoBehaviour {
-   
+
+    public Material neutral;
+    public Material p1;
+
+    [SerializeField]
+    private int status = 0;
+
 	// Use this for initialization
 	void Start () {
        
@@ -11,6 +17,22 @@ public class IslandBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ColorPicker();
 	}
+
+    void ColorPicker()
+    {
+        switch(status)
+        {
+            case 0:
+                this.gameObject.GetComponent<Renderer>().material = neutral;
+                break;
+            case 1:
+                this.gameObject.GetComponent<Renderer>().material = p1;
+                break;
+        }
+    }
+
+    public void setStatus(int i) { status = i; }
+    public int getSatus() { return status; }
 }
---
+++ GGJ2017/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject pos;
+    [SerializeField] 
+    private float jumpDist = 5f;
+
+	// Use this for initialization
+	void Start () {
+        pos.GetComponent<IslandBehavior>().setStatus(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handleInput();
+	}
+
+    void handleInput()
+    {
+        Vector2 ray = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        ray.Normalize();
+        RaycastHit2D[] hits = Physics2D.RaycastAll(pos.transform.position, ray);
+        Debug.DrawRay(pos.transform.position, ray, Color.green, 0.1f);
+        Debug.Log(ray); 
+
+        //bool check = false;
+        foreach (var hit in hits)
+        {
+
+            
+            if (hit.collider.gameObject.tag == "Island")
+            {
+                Debug.Log(hit.collider.gameObject.tag);
+                Debug.Log("hey");
+                switchToIsland(hit.collider.gameObject);
+            }
+        }
+    }
+
+    void switchToIsland(GameObject island)
+    {
+        pos.GetComponent<IslandBehavior>().setStatus(0);
+        pos = island;
+        pos.GetComponent<IslandBehavior>().setStatus(1);
+    }
+}
